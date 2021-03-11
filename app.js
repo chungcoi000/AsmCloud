da6fb84d29d237d5d8ceba45e508bf46098be208
chungcoi000 <chungntgch190246@fpt.edu.vn>
2021-03-11 14:48:32 +0700

    make it better

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var app = express()
 
 var MongoClient = require('mongodb').MongoClient;
 
-var url = 'mongodb://localhost:27017';
-//var url =  "mongodb+srv://chungcoi000:123456abc@cluster0.lkrga.mongodb.net/test";
+//var url = 'mongodb://localhost:27017';
 var url =  "mongodb+srv://chungcoi000:123456abc@cluster0.qhgiq.mongodb.net/toyDB";
 
 var publicDir = require('path').join(__dirname,'/public');
